f3d509372c8fb4641dde93076750ba6095167dd5
Luiz Filipi <115494778+Luiz005fil@users.noreply.github.com>
2022-10-26 16:23:58 -0300

    Material escolar
    Usando o Visualstudio

---
+++ MaterialEscolar/MaterialEscolar/Program.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Models.Caneta caneta = new Models.Caneta("Preto","Bic",100,"Plastico");
+
+        caneta.escrever("Luiz");
+        //caneta.exibir();
+
+        Lapis lapis = new Lapis("2B");
+        lapis.QuebrarPonta();
+        lapis.Escrever("Teste");
+
+    }
+}
